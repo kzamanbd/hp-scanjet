c53644a34e2d9cbd4c5052f945195d746bdd3808
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-02-01 00:28:03 +0600

    Initial commit

--- usb.js
+++ usb.js
@@ -4,9 +4,31 @@ const usb = require("usb");
 const VENDOR_ID = 1008; // HP
 const PRODUCT_ID = 24581; // ScanJet Pro 2500 f1
 
-const scannerDevice = usb.findByIds(VENDOR_ID, PRODUCT_ID);
+const term = usb.findByIds(VENDOR_ID, PRODUCT_ID);
 
-if (!scannerDevice) {
-    console.error("Scanner not found");
-    process.exit(1);
-}
+term.open();
+
+var endpoints = term.interfaces[0].endpoints,
+    inEndpoint = endpoints[0],
+    outEndpoint = endpoints[1];
+
+inEndpoint.transferType = 2;
+inEndpoint.startStream(1, 64);
+inEndpoint.transfer(64, function (error, data) {
+    if (!error) {
+        console.log(data);
+    } else {
+        console.log(error);
+    }
+});
+inEndpoint.on("data", function (data) {
+    console.log(data);
+});
+inEndpoint.on("error", function (error) {
+    console.log(error);
+});
+outEndpoint.transferType = 2;
+outEndpoint.startStream(1, 64);
+outEndpoint.transfer(new Buffer("d\n"), function (err) {
+    console.log(err);
+});
